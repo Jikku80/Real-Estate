fa859ab23c21363f7b79896bf4690fd66e0f3498
Jeshan Maharjan <47115629+Jikku80@users.noreply.github.com>
2021-07-14 12:02:54 +0545

    Update settings.py

--- realstateproject/settings.py
+++ realstateproject/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ad4b9kt(!obs1(w964ai!0f7ng=p0!xdqg5d$1_^v2p+o(sd7e'
+SECRET_KEY = 'ADD SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,8 +84,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hellruler80@gmail.com'
-EMAIL_HOST_PASSWORD = 'hqevczjyagtfvaib'
+EMAIL_HOST_USER = 'dummy@gmail.com'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_USE_TLS = True
 
 # Password validation
