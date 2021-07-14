684088057bcb4c99cc22f91bc98395eeb23ccb10
Jeshan Maharjan <47115629+Jikku80@users.noreply.github.com>
2021-07-14 12:03:52 +0545

    Update settings.py

--- realstateproject/settings.py
+++ realstateproject/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'real_estate',
         'USER': 'postgres',
-        'PASSWORD': 'Ai20@bBa1k',
+        'PASSWORD': '********',
         'HOST': 'localhost',
     }
 }
